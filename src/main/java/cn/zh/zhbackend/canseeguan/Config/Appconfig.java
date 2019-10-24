8e57f8596fc183535195f485278578116fc24ef8
hxx <2608875917@qq.com>
2019-10-24 15:24:43 +0800

    1024

---
+++ src/main/java/cn/zh/zhbackend/canseeguan/Config/Appconfig.java
@@ -0,0 +1,17 @@
+package cn.zh.zhbackend.canseeguan.Config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+@Component
+@PropertySource("classpath:application.yml")
+public class Appconfig {
+    @Value("${pdfRootPath}")
+    private String pdfRootPath;
+
+    public String getPdfRootPath() {
+        return pdfRootPath;
+    }
+
+}
--- src/main/java/cn/zh/zhbackend/canseeguan/service/DataService.java
+++ src/main/java/cn/zh/zhbackend/canseeguan/service/DataService.java
@@ -158,4 +158,7 @@ public class DataService {
 
         return res;
     }
+
+
+
 }
