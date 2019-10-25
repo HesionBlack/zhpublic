4d94c6ed7537d1e52f6107be79255bfb09f952d1
HesionBlack <1026320617@qq.com>
2019-10-25 09:01:16 +0800

    项目整合，冲突解决--1

--- src/main/java/cn/zh/zhbackend/canseeguan/Utils/AESUtil.java
+++ src/main/java/cn/zh/zhbackend/canseeguan/Utils/AESUtil.java
@@ -14,7 +14,7 @@ import java.nio.charset.StandardCharsets;
 public class AESUtil {
     //加密
     public static String aesEncrypt(String str, String key) throws Exception {
-        if (str == null || key == null) return null;
+        if (str == null || key == null) {return null;}
         Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
         cipher.init(Cipher.ENCRYPT_MODE, new SecretKeySpec(key.getBytes(StandardCharsets.UTF_8), "AES"));
         byte[] bytes = cipher.doFinal(str.getBytes(StandardCharsets.UTF_8));
@@ -22,47 +22,11 @@ public class AESUtil {
     }
     //解密
     public static String aesDecrypt(String str, String key) throws Exception {
-        if (str == null || key == null) return null;
+        if (str == null || key == null) {return null;};
         Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
         cipher.init(Cipher.DECRYPT_MODE, new SecretKeySpec(key.getBytes("utf-8"), "AES"));
         byte[] bytes = new BASE64Decoder().decodeBuffer(str);
         bytes = cipher.doFinal(bytes);
         return new String(bytes, "utf-8");
     }
-
-
-
-    /加密
-    public static String AESEncrypt(String content, String key) throws Exception {
-        if (key == null) {
-            System.out.print("Key不能为空null");
-            return null;
-        }
-        if (key.length() != 16) {
-            System.out.print("Key的长度不是16位");
-            return null;
-        }
-        Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
-        cipher.init(Cipher.ENCRYPT_MODE, new SecretKeySpec(key.getBytes(StandardCharsets.UTF_8), "AES"));
-        byte[] bytes = cipher.doFinal(content.getBytes(StandardCharsets.UTF_8));
-        return new BASE64Encoder().encode(bytes);
-    }
-
-    //解密
-    public static String AESDecrypt(String content, String key) throws Exception {
-        if (key == null) {
-            System.out.print("Key不能为空null");
-            return null;
-        }
-        if (key.length() != 16) {
-            System.out.print("Key的长度不是16位");
-            return null;
-        }
-        Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
-        cipher.init(Cipher.DECRYPT_MODE, new SecretKeySpec(key.getBytes("utf-8"), "AES"));
-        byte[] bytes = new BASE64Decoder().decodeBuffer(content);
-        bytes = cipher.doFinal(bytes);
-        return new String(bytes, "utf-8");
-    }
-
 }
